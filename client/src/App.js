a0b8191d49df30a5b1560a5bdedfeb69458cd441
tomertv2 <tomertv2@gmail.com>
2020-09-15 15:05:18 +0300

    Make basic React Router app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Song from './components/Song';
+import Album from './components/Album';
+import Playlist from './components/Playlist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h2>Music Streaming Service</h2>
+        <ul className="navbar">
+          <li><Link to={'/'} className="nav-link"> Home </Link></li>
+          <li><Link to={'/song'} className="nav-link">Song</Link></li>
+          <li><Link to={'/album'} className="nav-link">Album</Link></li>
+          <li><Link to={'/playlist'} className="nav-link">Playlist</Link></li>
+        </ul>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/song' component={Song} />
+            <Route path='/album' component={Album} />
+            <Route path='/Playlist' component={Playlist} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Album() {
+  return (
+    <div>
+        Album
+    </div>
+  );
+}
+
+export default Album;
