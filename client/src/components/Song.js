199d8494f01ee0b9ad98b608fef0e2e5e9620494
tomertv2 <tomertv2@gmail.com>
2020-09-21 22:46:48 +0300

    Style song page

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import axios from 'axios';
+import './Song.css';
 
 function Song() {
   const [songData, setSongData] = useState()
@@ -45,19 +46,23 @@ function Song() {
   return (
     <div>
       {songData && listOfSongs ?
-      <div className="song-container">
-        <h3>{songData.title}</h3>
-        <h4>{songData.artist_name} | {songData.album_name}</h4>
-        <iframe title="song-video" width="560" height="315" 
-        src={`https://www.youtube.com/embed/${songData.youtube_link}`} 
-        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        <div className="lyrics">{songData.lyrics}</div>
-        <div className="song-list-container">
-          {listOfSongs.map(song =>
-            <div key={song.song_id}>
-              <Link to={`/song/${song.song_id}${location.search}`}>{song.title}</Link> | {song.length} | {song.artist_name}
+      <div>
+        <h3 className="song-header">{songData.title}</h3>
+        <h4 className="mini-header">{songData.artist_name} | {songData.album_name}</h4>
+        <div className="song-data-container">
+          <div className="not-lyrics">
+            <iframe title="song-video" width="560" height="315" 
+            src={`https://www.youtube.com/embed/${songData.youtube_link}`} 
+            frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <div className="song-list-container">
+              {listOfSongs.map(song =>
+                <div key={song.song_id}>
+                  <Link to={`/song/${song.song_id}${location.search}`}>{song.title}</Link> | {song.length} | {song.artist_name}
+                </div>
+              )}
             </div>
-          )}
+          </div>
+          <div className="lyrics">{songData.lyrics}</div>  
         </div>
       </div> :
       <div>no song found</div>}
