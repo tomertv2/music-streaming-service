32cc51f49537fbb03274b3e2fc1c273900097532
tomertv2 <tomertv2@gmail.com>
2020-09-21 21:16:23 +0300

    404 error handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Home from './components/Home';
 import Song from './components/Song';
 import Album from './components/Album';
 import Playlist from './components/Playlist';
 import Artist from './components/Artist';
 import NavBar from './components/NavBar';
+import Error404 from './components/Error404';
+import './App.css';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path='/album/:id' component={Album} />
             <Route path='/Playlist/:id' component={Playlist} />
             <Route path='/artist/:id' component={Artist} />
+            <Route component={Error404} />
         </Switch>
       </div>
     </Router>
