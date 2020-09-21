2bd840667f268bc413c2b58baa8fd86c24b6923e
tomertv2 <tomertv2@gmail.com>
2020-09-21 22:29:02 +0300

    Style album page

--- client/src/components/Album.js
+++ client/src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+import './Album.css';
 
 function Album() {
   const [album, setAlbum] = useState({});
@@ -21,15 +22,17 @@ function Album() {
     <div>
         {album && songsInAlbum ? 
         <div className="album-container">
-            <h3>{album.album_name}</h3>
-            <h4>{album.artist_name}</h4>
-            <img src={album.cover_img} alt={album.album_name} width='300' height='300' />
-            <div>
-            {songsInAlbum.map(song => 
-              <div key={song.song_id} className="song-in-list">
-                <Link to={`/song/${song.song_id}?album=${params.id}`}>{song.title}</Link> | {song.length}
-              </div>  
-            )}
+            <h3 className="album-header">{album.album_name}</h3>
+            <h4 className="art-header">By {album.artist_name}</h4>
+            <div className="album-data-container">
+              <img src={album.cover_img} alt={album.album_name} width='300' height='300' />
+              <div className="song-list">
+                {songsInAlbum.map(song => 
+                  <div key={song.song_id}>
+                    <Link to={`/song/${song.song_id}?album=${params.id}`}>{song.title}</Link> | {song.length}
+                  </div>  
+                )}
+              </div>
             </div>
         </div> :
         <div>no album found</div>}
