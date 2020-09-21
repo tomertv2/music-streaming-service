44756d6f7d6e28fd32765a6b5bb0a43df8cf6948
tomertv2 <tomertv2@gmail.com>
2020-09-21 22:13:13 +0300

    Style playlist page

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+import './Playlist.css';
 
 function Playlist() {
   const [playlist, setPlaylist] = useState({});
@@ -20,14 +21,21 @@ function Playlist() {
     <div>
         {playlist[0] ? 
         <div className="playlist-container">
-            <div>{playlist[0].playlist_name}</div>
-            <div>{playlist[0].created_at.replace(/T/g, ' ').replace(/Z/g, '')}</div>
-            <img src={playlist[0].cover_img} alt={playlist[0].playlist_name} width='300' height='300' />
-            {playlist.map(song => 
-              <div key={song.songs_in_playlists_id}>
-                <Link to={`/song/${song.song_id}?playlist=${params.id}`}>{song.title}</Link> | {song.artist_name} | {song.length}
+            <div className="playlist-header">{playlist[0].playlist_name}</div>
+            <div className="playlist-data-container">
+              <div className="playlist-photo-time">
+                <img src={playlist[0].cover_img} alt={playlist[0].playlist_name} width='300' height='300' />
+                <br />
+                <div>Created At: {playlist[0].created_at.replace(/T/g, ' ').replace(/Z/g, '')}</div>
               </div>
-            )}
+              <div className="song-list">
+                {playlist.map(song => 
+                  <div key={song.songs_in_playlists_id}>
+                    <Link to={`/song/${song.song_id}?playlist=${params.id}`}>{song.title}</Link> | {song.artist_name} | {song.length}
+                  </div>
+                )}
+              </div>
+            </div>
         </div> :
         <div>no playlist found</div>}
     </div>
