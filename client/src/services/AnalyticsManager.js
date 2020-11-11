1f6d6727de5ba9fc6401a880e098eaac6f2f19b5
tomertv2 <tomertv2@gmail.com>
2020-11-11 17:10:14 +0200

    Update AnalyticsManager.js

--- client/src/services/AnalyticsManager.js
+++ client/src/services/AnalyticsManager.js
@@ -3,6 +3,6 @@ import mixpanel from 'mixpanel-browser';
 mixpanel.init('f30974166014d5ba6546aa81514e6723');
 
 export default function track(eventName, options = {}) {
-    console.log('data passed')
+  console.log('data passed');
   return mixpanel.track(eventName, options);
 }
